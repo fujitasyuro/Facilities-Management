04a5b82227c86b05872ec8a36282168fd5a6b704
Fujita Syuro <syuro.dev@gmail.com>
2022-03-26 17:23:33 +0700

    up 2

--- JS/roomOpen.js
+++ JS/roomOpen.js
@@ -3,7 +3,8 @@ var buildingBtns = document.querySelectorAll(".miniroom");
 buildingBtns.forEach(function(btn) {
    btn.onclick = function() {
       var floor = btn.getAttribute("data-modal");
-
+      document.getElementById("rooms").style.display = "block";
+      // document.getElementById(floor).classList.add("active");
       document.getElementById(floor).style.display = "block";
    };
 });
@@ -13,6 +14,6 @@ var buildingClose = document.querySelectorAll(".room--close");
 buildingClose.forEach(function(btn) {
    btn.onclick = function() {
       var room = (btn.closest(".rooms__room").style.display = "none");
-      console.log("click");
+      document.getElementById("rooms").style.display = "none";
    };
 });
